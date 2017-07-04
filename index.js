a72b852fdcddc4cb07f06bf344f6894e63072bd9
Ahmong <ahmong@126.com>
2017-07-04 22:39:54 +0800

    Remove debug statement.

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ module.exports = loader;
 function loader(content) {
   try {
     return content;
-    // return csslocalsFromJsLoader.bind(this)(content);
   } catch (err) {
     console.log("Error in csslocals-from-js-loader/index.js :");
     console.log(err.stack);
@@ -15,45 +14,8 @@ function loader(content) {
   }
 }
 
-/*
-function csslocalsFromJsLoader(content) {
-  console.log("Begin of csslocalsFromJsLoader()");
-  console.log("request=", this.request);
-  console.log("content='''\n", content, "\n'''");
-  var query = _loaderUtils2.getOptions(this) || {};
-
-  var exportKeys = ['locals'];
-  if (query.exports) {
-    if (Array.isArray(query.exports)) {
-      exportKeys = exportKeys.concat(query.exports)
-    } else if (typeof query.exports === 'string') {
-      exportKeys.push(query.exports)
-    }
-  }
-
-  if (this.cacheable) this.cacheable();
-
-  console.log('resourcePath= ', this.resourcePath);
-
-  var exports = this.exec(content, this.resourcePath);
-  var exportLocals = {};
-
-  for (expKey of exportKeys) {
-    if (exports[expKey]) {
-      Object.assign(exportLocals, exports[expKey]);
-    }
-  }
-
-  var result = "exports.locals = " + JSON.stringify(exportLocals);
-
-  console.log("csslocalsFromJsLoader result= '''\n" + result + "\n'''");
-  return result;
-}
-*/
-
 module.exports.pitch = function(remainingRequest) {
-  console.log('Begin in csslocals-from-js-loader/loader.pitch(), resource= ',
-    path.basename(this.resource));
+  // console.log('Begin in csslocals-from-js-loader/loader.pitch(), resource= ', path.basename(this.resource));
   // console.log('remainingRequest=', remainingRequest);
 
   try {
@@ -77,8 +39,6 @@ module.exports.pitch = function(remainingRequest) {
       '',
       '// load the JS file',
       'var content = require(' + request + ');',
-      // content list format is [id, css, media, sourceMap]
-      // "if(typeof content === 'string') content = [[module.id, content, '']];",
       'var exportLocals = {};',
       `for (expKey of ${JSON.stringify(exportKeys)}) {`,
       '  if (content[expKey]) {',
@@ -86,11 +46,10 @@ module.exports.pitch = function(remainingRequest) {
       '  }',
       '}',
       'exports.locals = exportLocals;'
-      // 'if(content.locals) module.exports = content.locals;'
     ]
     var resultStr = resultJs.join('\n')
     // console.log('resultStr=***\n', resultStr, '\n***');
-    console.log('End of csslocals-from-js-loader/loader.pitch()');
+    // console.log('End of csslocals-from-js-loader/loader.pitch()');
     return resultStr;
   } catch (err) {
     console.log("Error in csslocals-from-js-loader/loader.pitch():");
